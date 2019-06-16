88ff4e7db2d6e3a480ba351fb4a977b448772c19
Hurlyburly2 <dougdougmann@gmail.com>
2019-06-16 15:05:10 -0400

    finished lesson

--- src/com/dsimon/Main.java
+++ src/com/dsimon/Main.java
@@ -3,6 +3,15 @@ package com.dsimon;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // width of in = 32 (4 bytes)
+	    int myIntValue = 5 / 3;
+	    // width of float = 32 (4 bytes)
+	    float myFloatValue = 5f / 3f;
+	    // width of double = 64 (8 bytes) (mostly use double over float)
+	    double myDoubleValue = 5d / 3d;
+
+        System.out.println("myIntValue = " + myIntValue);
+        System.out.println("myFloatValue = " + myFloatValue);
+        System.out.println("myDoubleValue = " + myDoubleValue);
     }
 }
